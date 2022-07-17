a654df2a10df897150867f424bd44e137e9b622e
allan <shuttxx09@gmail.com>
2022-07-17 13:13:00 -0300

    python-fun-es...modulariza-o...erros

--- 009.py
+++ 009.py
@@ -1,1 +1,15 @@
-def
+def ficha(jog='<desconhecido>', gol=0):
+    print(f'O jogador {jog} fez {gol} gol(s) no campeonato')
+
+
+# programa principal
+n = str(input('Nome do Jogador: '))
+g = str(input('Numero de Gols: '))
+if g.isnumeric():
+    g = int(g)
+else:
+    g=0
+if n.strip() == '':
+    ficha(gol=g)
+else:
+    ficha(n, g)
---
+++ 011.py
@@ -0,0 +1,19 @@
+def notas(*n, sit=False):
+    r = dict()
+    r['Total'] = len(n)
+    r['Maior'] = max(n)
+    r['Menor'] = min(n)
+    r['Média'] = sum(n)/len(n)
+    if sit:
+        if r['Média'] >= 7:
+            r['Situação'] = 'BOA'
+        elif r['Média'] >= 5:
+            r['Situação'] = 'RAZOÁVEL'
+        else:
+            r['Situação'] = 'RUIM'
+    return r
+
+
+# programa principal
+resp = notas(5.5, 2.5, 8.5, sit=True)
+print(resp)
